0e018b4d256524e50a24aea7b4d20109dcbfdd5c
Julio Cesar <juliocesargualberto617@gmail.com>
2024-08-11 01:20:39 -0300

    add logout function

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -3,8 +3,13 @@ import {
   IconBuildingStore,
   IconLogout,
 } from "@tabler/icons-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export function Dashboard({ children }: any) {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <>
       <div className="w-full">
@@ -36,9 +41,12 @@ export function Dashboard({ children }: any) {
               </div>
             </div>
             <div className="font-medium flex p-5 gap-4 justify-start items-center text-zinc-50 h-20 border border-stone-800  border-t-stone-700 ">
-              <div className="flex items-center justify-center rounded-md w-10 h-10 bg-blue-600/20">
+              <button
+                onClick={logout}
+                className="flex items-center cursor-pointer justify-center rounded-md w-10 h-10 bg-blue-600/20"
+              >
                 <IconLogout color="#1289f8" size={25} />
-              </div>
+              </button>
               Logout
             </div>
           </nav>
